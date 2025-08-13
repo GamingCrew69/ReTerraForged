31ed10570d07cf31737642b0dd520d6024d02e3b
Werner, Colin <crwgond@gmail.com>
2025-08-12 20:55:01 -0500

    fixed tags, customize screen doesn't crash anymore

--- common/src/main/java/raccoonman/reterraforged/data/worldgen/tags/RTFDensityFunctionTagsProvider.java
+++ common/src/main/java/raccoonman/reterraforged/data/worldgen/tags/RTFDensityFunctionTagsProvider.java
@@ -21,9 +21,10 @@ public class RTFDensityFunctionTagsProvider extends TagsProvider<DensityFunction
 	protected void addTags(HolderLookup.Provider provider) {
 //		this.tag(RTFDensityFunctionTags.ADDITIONAL_NOISE_ROUTER_FUNCTIONS).add(PresetNoiseRouterData.GRADIENT, PresetNoiseRouterData.HEIGHT_EROSION, PresetNoiseRouterData.SEDIMENT, TBNoiseRouterData.UNIQUENESS);
 		this.getOrCreateRawBuilder(RTFDensityFunctionTags.ADDITIONAL_NOISE_ROUTER_FUNCTIONS)
-				.addTag(PresetNoiseRouterData.GRADIENT.location())
-				.addTag(PresetNoiseRouterData.HEIGHT_EROSION.location())
-				.addTag(PresetNoiseRouterData.SEDIMENT.location())
-				.addTag(TBNoiseRouterData.UNIQUENESS.location());
+				.addElement(PresetNoiseRouterData.GRADIENT.location())
+				.addElement(PresetNoiseRouterData.HEIGHT_EROSION.location())
+				.addElement(PresetNoiseRouterData.SEDIMENT.location())
+				.addElement(TBNoiseRouterData.UNIQUENESS.location());
+
 	}
 }
